db0fb0b6701e5473e4881e014f526d06a251069c
Peter Prog <peter.prog@yahoo.com>
2022-08-07 20:48:00 +0800

    api: kleine änderungen

--- api/api_handler.js
+++ api/api_handler.js
@@ -27,9 +27,10 @@ const server = new ApolloServer({
   },
 });
 
-function installHandler(app) {
+async function installHandler(app) {
   const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
   console.log('CORS setting:', enableCors);
+  await server.start();
   server.applyMiddleware({ app, path: '/graphql', cors: enableCors });
 }
 
